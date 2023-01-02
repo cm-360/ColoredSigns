5777c28334a74268d338ed3e13508bc70fcf6f89
cm360 <cm360@tron>
2023-01-02 18:28:25 -0500

    Add disabling functionality

--- src/com/github/cm360/coloredsigns/main/ColoredSignsPlugin.java
+++ src/com/github/cm360/coloredsigns/main/ColoredSignsPlugin.java
@@ -1,19 +1,26 @@
 package com.github.cm360.coloredsigns.main;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.cm360.coloredsigns.listeners.SignChangeListener;
 
 public class ColoredSignsPlugin extends JavaPlugin {
 
+	protected SignChangeListener listener;
+	
+	public ColoredSignsPlugin() {
+		listener = new SignChangeListener();
+	}
+	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new SignChangeListener(), this);
+		getServer().getPluginManager().registerEvents(listener, this);
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		HandlerList.unregisterAll(this);
 	}
 
 }
